07016aa10c241bac356ef5650aaa66b03f5063e9
sk1@assaycr.com <satish.achary1991@gmail.com>
2020-08-21 12:06:15 +0530

    fixed unsupported annotation which is removed in spring 2.0v

--- src/test/java/com/fms/core/FmsApplicationTests.java
+++ src/test/java/com/fms/core/FmsApplicationTests.java
@@ -3,11 +3,12 @@ package com.fms.core;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = FmsApplication.class)
+@SpringBootTest(classes = FmsApplication.class)
 @WebAppConfiguration
 public class FmsApplicationTests {
 
